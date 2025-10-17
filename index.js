4199fdf8abdd2868facaefff03242423516d1334
Fazlie <93725034+Fazlie14@users.noreply.github.com>
2025-10-18 05:50:09 +0800

    making a function of start game

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = ""
 
-if(sum <= 20){
-  message = "Do you want to draw a new card?"
-  
-}else if(sum === 21){
-  message = "Wohooo! You've got a blackjack!"
-  hasBlackJack = true;
-}else{
-  message = "You're out of the game"
-  isAlive = false
+
+function startGame(){
+  if(sum <= 20){
+    message = "Do you want to draw a new card?"
+    
+  }else if(sum === 21){
+    message = "Wohooo! You've got a blackjack!"
+    hasBlackJack = true;
+  }else{
+    message = "You're out of the game"
+    isAlive = false
+  }
 }
-console.log(isAlive)
-console.log(message)
+
