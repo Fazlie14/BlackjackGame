debf2650d6a56f538c8eeb71c50bb73d51487f14
Fazlie <93725034+Fazlie14@users.noreply.github.com>
2025-10-18 06:23:11 +0800

    done making the function of start button and display the result in the P tag

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 let firstCard = 10;
-let secondCard = 9;
+let secondCard = 11;
 
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = ""
+let messageEl = document.getElementById("messg-El");
+let sumEl = document.querySelector("#sumEl");
+let cardsEl = document.querySelector("#card-El");
 
 
 function startGame(){
+  cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
+  sumEl.textContent = `Sum: ${sum}`;
   if(sum <= 20){
     message = "Do you want to draw a new card?"
     
   }else if(sum === 21){
-    message = "Wohooo! You've got a blackjack!"
+    message = "You've got a blackjack!"
     hasBlackJack = true;
   }else{
     message = "You're out of the game"
     isAlive = false
   }
+  messageEl.textContent = message;
+  
 }
 
